80d5b88d19e7adf85603bae68c6a01e1d9ccd17e
pranavraagz <pranavraagz@gmail.com>
2021-11-27 21:49:37 +0530

    fix(test): change test input to match new main()

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:timer_task_nearcast/main.dart';
+import 'package:timer_task_nearcast/app.dart';
+import 'package:timer_task_nearcast/tasks_repository.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(App(tasksRepository: TasksRepository()));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
